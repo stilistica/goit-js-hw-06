a40cd062cf41002eb79bf0a5f890ec5d09825f1f
stilistica <119519958+stilistica@users.noreply.github.com>
2023-04-14 19:18:49 +0300

    task_8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginFormEl = document.querySelector("form.login-form");
 
+loginFormEl.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const emailInput = loginFormEl.elements.email;
+	const passwordInput = loginFormEl.elements.password;
+
+	if (!emailInput.value || !passwordInput.value) {
+		alert(`Всі поля повинні бути заповнені.`)
+		return;
+	} 
+	const formData = {
+		email: emailInput.value,
+		password: passwordInput.value,
+	}
+
+	console.log(formData)
+
+	loginFormEl.reset();
+});
