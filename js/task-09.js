ede0ef2b04ca6ff6abeddc9747d4300fe9b891ac
stilistica <119519958+stilistica@users.noreply.github.com>
2023-04-14 19:26:38 +0300

    task_9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const btnEl = document.querySelector("button.change-color");
+const spanEl = document.querySelector("span.color");
+
+btnEl.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+})
