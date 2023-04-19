d5449a4974444f6d504a6ed756e276bbaf12151a
stilistica <119519958+stilistica@users.noreply.github.com>
2023-04-19 17:07:24 +0300

    доопрацювання task_5

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,5 @@ const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
 
 inputEl.addEventListener("input", () => {
-	outputEl.textContent = inputEl.value || "Anonymous";
+	outputEl.textContent = inputEl.value.trim() || "Anonymous";
 });
