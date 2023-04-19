c1351bfb3f13962785754088dcc8b8659ccd86cd
stilistica <119519958+stilistica@users.noreply.github.com>
2023-04-19 17:28:02 +0300

    доопрацювання task_7

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const sizeControleEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
-
+textEl.style.fontSize = `${sizeControleEl.value}px`;
 sizeControleEl.addEventListener("input", () => {
 	text.style.fontSize = `${sizeControleEl.value}px`;
 });
