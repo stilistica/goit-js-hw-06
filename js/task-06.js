e0e6f3e05d5cfc7ce933f6b092d0fb023cc4e510
stilistica <119519958+stilistica@users.noreply.github.com>
2023-04-19 17:21:56 +0300

    доопрацювання task_6

--- js/task-06.js
+++ js/task-06.js
@@ -3,12 +3,10 @@ const validationInput = document.querySelector("#validation-input");
 validationInput.addEventListener("blur", () => {
 	const expectedLength = Number(validationInput.dataset.length);
 	const factLength = validationInput.value.length;
+	validationInput.classList.remove("valid", "invalid");
 
-	if (factLength === expectedLength) {
-		validationInput.classList.add("valid");
-		validationInput.classList.remove("invalid");
-	} else {
-		validationInput.classList.add("invalid");
-		validationInput.classList.remove("valid");
-	}
+	factLength === expectedLength
+		? validationInput.classList.add("valid")
+		: validationInput.classList.add("invalid");
 });
+
